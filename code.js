8d1254fe517431e18e2e7e5cc4c4e8f04db0b366
EAndriano <a01747979@tec.mx>
2023-04-08 19:52:12 -0600

    Set function getPlayerChoice

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
+
+function getPlaterChoice() {
+    let answer = prompt('Whats your choice (Rock/Paper/Scissors)?')
+    if (answer.toLowerCase() === 'rock') {
+        return 0;
+    } else if (answer.toLowerCase() === 'paper') {
+        return 1;
+    } else if (answer.toLowerCase() === 'scissors') {
+        return 2;
+    } else {
+        alert('Error. You must select a valid option.')
+    }
+}
