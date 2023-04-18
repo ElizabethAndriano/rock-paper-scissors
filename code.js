86c7b937c489079f49ac01c49951622c3d08479d
EAndriano <a01747979@tec.mx>
2023-04-18 09:50:52 -0600

    Change game display from console to graphic ui

--- code.js
+++ code.js
@@ -2,20 +2,6 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3)
 }
 
-/*function getPlayerChoice() {
-    const answer = prompt('Whats your choice (Rock/Paper/Scissors)?')
-    if (answer.toLowerCase() === 'rock') {
-        return 0
-    } else if (answer.toLowerCase() === 'paper') {
-        return 1
-    } else if (answer.toLowerCase() === 'scissors') {
-        return 2
-    } else {
-        alert('Error. You must select a valid option.')
-        return getPlayerChoice()
-    }
-}*/
-
 function round(playerSelection) {
     const computerSelection = getComputerChoice()
     
