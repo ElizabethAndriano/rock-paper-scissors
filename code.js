9c6150790e825cb3500ffbf1b2293666607cd2d1
EAndriano <a01747979@tec.mx>
2023-04-18 10:20:56 -0600

    Limit to 5 points

--- code.js
+++ code.js
@@ -49,20 +49,32 @@ function round(playerSelection) {
 
     pScore.textContent = playerScore
     cScore.textContent = computerScore
+
+    if (playerScore === 5) {
+        document.querySelector('h2.result').textContent = 'You Win!!!'
+        buttons.forEach((button) => {
+            button.disabled = true    
+        })
+    } else if (computerScore === 5) {
+        document.querySelector('h2.result').textContent = 'You Lose'
+        buttons.forEach((button) => {
+            button.disabled = true    
+        })
+    }
 }
 
 let computerScore = 0
 let playerScore = 0
 
-const keys = document.querySelectorAll(".option");  
+const buttons = document.querySelectorAll(".option");  
 
-keys.forEach( (key) => {
-    key.addEventListener('click', (e) =>{
-        if (key.id === 'rock') {
+buttons.forEach( (button) => {
+    button.addEventListener('click', (e) =>{
+        if (button.id === 'rock') {
             result = round(0);
-        } else if (key.id === 'paper'){
+        } else if (button.id === 'paper'){
             result = round(1)
-        } else if (key.id === 'scissors'){
+        } else if (button.id === 'scissors'){
             result = round(2)
         } else return;
     })
